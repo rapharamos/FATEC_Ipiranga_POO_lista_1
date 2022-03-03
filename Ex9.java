5b15f65c4a8bca0e57eb7916263b03b162fc095d
‘ola’ <ola>
2022-03-03 14:21:11 -0300

    Adicionado exercicio 9

--- Ex9.java
+++ Ex9.java
@@ -1,3 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class Ex9 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        double salario, reajuste, salariof;
+        //entrada de dados
+        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário do funcionário:"));
+        reajuste = Double.parseDouble(JOptionPane.showInputDialog("Digite o percentual de reajuste:"));
+        //processamento
+        salariof = salario + (salario * (reajuste/100));
+        //saía de dados
+        String s = String.format("Salário novo será de %.2f:", salariof);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
