7f6f52ed20f6335fb313c3d18a5fd318ce4ad9ab
‘ola’ <ola>
2022-03-03 14:34:36 -0300

    Exercicio 11 adicionado e lista finalizada

--- Ex11.java
+++ Ex11.java
@@ -1,3 +1,19 @@
+import javax.swing.JOptionPane;
+
 public class Ex11 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        double quantidade, valor, salariofixo, bonusfixo, salariofinal;
+        //entrada de dados
+        quantidade = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade de carros vendidos:"));
+        valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor total em vendas:"));
+        salariofixo = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário do funcionário:"));
+        bonusfixo = Double.parseDouble(JOptionPane.showInputDialog("Digite a bonificação por carro vendido:"));
+        //processamento
+        salariofinal = salariofixo + (bonusfixo * quantidade) + (valor * 0.05);
+        //saía de dados
+        String s = String.format("O salário do vendedor será R$ %.2f:", salariofinal);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
