2f14ed4dca57e7aea7dced0a2037002911ce6d87
‘ola’ <ola>
2022-03-03 14:03:33 -0300

    Adiciondo exercicio 6

--- Ex6.java
+++ Ex6.java
@@ -1,3 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Ex6 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        int anos, dias;
+        //entrada de dados
+        anos = Integer.parseInt(JOptionPane.showInputDialog("Quantos anos você tem?"));
+        //processamento
+        dias = anos * 365;
+        //saía de dados
+        String s = String.format("Sua idade em dias é: %d", dias);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
