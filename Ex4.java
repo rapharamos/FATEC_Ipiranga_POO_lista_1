6c7a32e781b00be88bcbf0c1ebd2913a1b08b4f0
‘ola’ <ola>
2022-03-03 13:55:10 -0300

    Exercicio 4 finalizado

--- Ex4.java
+++ Ex4.java
@@ -1,3 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Ex4 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        int valor, antecessor;
+        //entrada de dados
+        valor = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro:"));
+        //processamento
+        antecessor = valor - 1;
+        //saía de dados
+        String s = String.format("Seu antecessor é: %d", antecessor);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
