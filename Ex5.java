05e4123f068326e881391f70f4643cbe976177b1
‘ola’ <ola>
2022-03-03 14:00:23 -0300

    Adicionado exercicio 5

--- Ex5.java
+++ Ex5.java
@@ -1,3 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class Ex5 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        double base, altura, area;
+        //entrada de dados
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base do retângulo:"));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do retângulo:"));
+        //processamento
+        area = base * altura;
+        //saía de dados
+        String s = String.format("Seu antecessor é: %.2f", area);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
