9829524d4780f49ca5025748f21ab1558d028760
‘ola’ <ola>
2022-03-03 14:35:50 -0300

    Atualizaçao e confirmaçao de todos os arquivos

--- Ex11.java
+++ Ex11.java
@@ -6,9 +6,9 @@ public class Ex11 {
         double quantidade, valor, salariofixo, bonusfixo, salariofinal;
         //entrada de dados
         quantidade = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade de carros vendidos:"));
-        valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor total em vendas:"));
-        salariofixo = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário do funcionário:"));
-        bonusfixo = Double.parseDouble(JOptionPane.showInputDialog("Digite a bonificação por carro vendido:"));
+        valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor total de vendas em reais:"));
+        salariofixo = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário do funcionário em reais:"));
+        bonusfixo = Double.parseDouble(JOptionPane.showInputDialog("Digite a bonificação por carro vendido em reais:"));
         //processamento
         salariofinal = salariofixo + (bonusfixo * quantidade) + (valor * 0.05);
         //saía de dados
