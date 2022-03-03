98a9e28047cc85b1f8be196c17991b34741e8e25
‘ola’ <ola>
2022-03-03 14:26:03 -0300

    Adicionado exercicio 10

--- Ex10.java
+++ Ex10.java
@@ -1,3 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Ex10 {
-    
+    public static void main(String[] args) {
+        //declaração de variaveis
+        double custo, distribuidor = 28, imposto = 45, preco;
+        //entrada de dados
+        custo = Double.parseDouble(JOptionPane.showInputDialog("Digite o custo de fábrica do veículo em reais:"));
+        //processamento
+        preco = custo + (custo * (distribuidor/100)) + (custo * (imposto/100));
+        //saía de dados
+        String s = String.format("O preço final do veículo será R$ %.2f:", preco);
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
 }
